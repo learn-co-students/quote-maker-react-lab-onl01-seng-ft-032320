091d026e9e9f0aab360f058aff35a2db16db9c3b
jrestrepo922 <tinto922@gmail.com>
2020-08-06 09:26:46 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,24 @@
-export default (state = [], action) => {
-  return state;
+
+ 
+ export default (state = [], action) =>{
+   let idx;
+  switch (action.type){
+    case "ADD_QUOTE":
+      return  state.concat(action.quote)
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId)   
+      
+    case "UPVOTE_QUOTE":
+      idx = state.findIndex(quote => quote.id === action.id)   
+      return  [...state.slice(0, idx), ...state.slice(idx +1)];
+    case "DOWNVOTE_QUOTE":
+      idx = state.findIndex(quote => quote.id === action.id)   
+      return  [...state.slice(0, idx), ...state.slice(idx +1)];
+    default: 
+       return state;
+  }
+    
+
+  
 }
+
